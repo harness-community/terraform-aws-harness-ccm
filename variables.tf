fad0ab1e45e240d10f622ac88e3ee5e75f0173b9
Nic Acton <awesome.acton@gmail.com>
2024-02-16 05:56:51 -0800

    "Make AWS Account ID variable for new cloud accounts and for better SMP onboarding experience" (#13)
    * "Make AWS Account ID variable for new cloud accounts and for better SMP onboarding experience"
    
    * Update variables.tf
    
    ---------
    
    Co-authored-by: Riley Snyder <rileysnyder@protonmail.ch>

--- variables.tf
+++ variables.tf
@@ -4,9 +4,15 @@ variable "s3_bucket_arn" {
   default     = ""
 }
 
+variable "aws_account_id" {
+  description = "AWS account ID"
+  type        = string
+  default     = "891928451355"
+}
+
 variable "external_id" {
   type        = string
-  description = "External ID given in the harness UI: harness:891928451355:<guid>"
+  description = "External ID given in the harness UI: harness:<aws_account_id>:<guid>"
 }
 
 variable "enable_billing" {
